1c85fdeb16e3307da248bff8eeb1b69de7d78bf9
Jon Kinney <jonkinney@gmail.com>
2011-09-22 03:07:45 -0500

    dotfiles fix

--- dotfiles.rb
+++ dotfiles.rb
@@ -3,7 +3,7 @@ meta 'dotfiles' do
   accepts_value_for :target, :basename
 
   template {
-    met? { "/Users/`whoami`/.#{target}/.git".p.dir? }
+    met? { "/Users/`whoami`/.j2fly-#{target}/".p.dir? }
     meet {
       log_shell "Cloning", "git clone git@github.com:j2fly/#{repo}.git /Users/`whoami`/.j2fly-#{target}"
       log "Symlinking"
