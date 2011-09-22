f49bdc7cfc39fc0676ae22cf5652b3e734a15244
Jon Kinney <jonkinney@gmail.com>
2011-09-22 03:48:02 -0500

    updates

--- oh_my_zsh.rb
+++ oh_my_zsh.rb
@@ -6,6 +6,7 @@ dep 'oh-my-zsh' do
   meet {
     log "Installing oh-my-zsh from http://github.com/robbyrussell/oh-my-zsh"
     shell 'sudo bash -c "`curl https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh`"'
+    log_shell "Symlinking ~/.j2fly-dotfiles/home/.zshrc to ~/.zshrc", "ln -nsf /Users/`whoami`/.j2fly-dotfiles/home/.zshrc /Users/`whoami`/.zshrc"
   }
 end
   
