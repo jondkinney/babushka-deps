bde516a473e9b5e985e1baab7f0a9f4e9bdb9547
Jon Kinney <jonkinney@gmail.com>
2011-09-22 03:19:58 -0500

    dotfiles fix

--- dotfiles.rb
+++ dotfiles.rb
@@ -3,8 +3,11 @@ meta 'dotfiles' do
   accepts_value_for :target, :basename
 
   template {
-    met? { "/Users/`whoami`/.j2fly-#{target}/".p.dir? }
+    met? { "/Users/`whoami`/.j2fly-#{target}/installed_successfully".p.file? }
     meet {
+      if "/Users/`whoami`/.j2fly-#{target}".p.exists?
+        log_shell "Removing existing .j2fly-dotfiles", "sudo rm -r ~/.j2fly-dotfiles"
+      end
       log_shell "Cloning", "git clone git@github.com:j2fly/#{repo}.git /Users/`whoami`/.j2fly-#{target}"
       log "Symlinking"
       raw_shell("cd /Users/`whoami`/.j2fly-#{target}/tools && sh install.sh").stdout.include?("All done")
