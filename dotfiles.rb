0589b086cf5b0cb22111790227ad1b62e32dd1be
Jon Kinney <jonkinney@gmail.com>
2011-09-22 04:26:10 -0500

    try to shellout logging stufs

--- dotfiles.rb
+++ dotfiles.rb
@@ -11,7 +11,7 @@ meta 'dotfiles' do
       if log_shell "Cloning", "git clone git@github.com:j2fly/#{repo}.git /Users/`whoami`/.j2fly-#{target}"
         log "Symlinking"
         shellout = raw_shell("cd /Users/`whoami`/.j2fly-#{target}/tools && sh install.sh").stdout
-        log shellout.to_s
+        log "#{shellout}"
         return true if shellout.include?("All done")
       end
     }
