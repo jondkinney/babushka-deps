9967c4a9d693590eae73de012179e8cb118a0260
Jon Kinney <jonkinney@gmail.com>
2011-09-23 06:21:32 -0500

    update omnigraffle to eula

--- app_bundles.rb
+++ app_bundles.rb
@@ -28,7 +28,7 @@ end
 
 dep 'OmniGraffle.app', :template => 'eula_app' do
   source 'http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.5/OmniGrafflePro-5.3.3.dmg'
-  dmg_name 'OmniGrafflePro-5.3.3'
+  dmg_name 'OmniGrafflePro'
 end
 
 dep 'Evernote.app', :template => 'eula_app' do
