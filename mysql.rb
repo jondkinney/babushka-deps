c9af8935e0b201fb577baa20f71d410646bbbcd0
Jon Kinney <jonkinney@gmail.com>
2011-09-23 17:06:09 -0500

    dbfixes

--- mysql.rb
+++ mysql.rb
@@ -8,7 +8,11 @@ end
 
 dep 'mysql configured' do
   requires 'mysql.managed'
-  log_shell "unset TMPDIR", "unset TMPDIR"
-  log_shell "Executing mysql_install_db with custom flags", 'sudo mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp'
-  log_shell "Starting MySQL", "mysql.server start"
+  
+  met?{ 1==2 }
+  meet{    
+    log_shell "unset TMPDIR", "unset TMPDIR"
+    log_shell "Executing mysql_install_db with custom flags", 'sudo mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp'
+    log_shell "Starting MySQL", "mysql.server start"
+  }
 end
