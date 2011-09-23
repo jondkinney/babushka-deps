bd262f23f95edb81ddc9d28738f32b19a430bba0
Jon Kinney <jonkinney@gmail.com>
2011-09-23 16:33:41 -0500

    dbfixes

--- mysql.rb
+++ mysql.rb
@@ -1,10 +1,11 @@
 dep 'mysql.managed' do
+  after {
+    unless "/usr/local/var/mysql".p.exists?
+      log "This is your first install"
+      log_shell "unset TMPDIR", "unset TMPDIR"
+      log_shell "Executing mysql_install_db with custom flags", 'sudo mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp'
+    end
 
-  unless "/usr/local/var/mysql".p.exists?
-    log "This is your first install"
-    log_shell "unset TMPDIR", "unset TMPDIR"
-    log_shell "Executing mysql_install_db with custom flags", 'sudo mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp'
-  end
-
-  log_shell "Starting MySQL", "mysql.server start"
+    log_shell "Starting MySQL", "mysql.server start"
+  }
 end
--- postgres.rb
+++ postgres.rb
@@ -1,10 +1,12 @@
 dep 'postgresql.managed' do
   provides "psql"
   
-  unless "/usr/local/var/postgres".p.exists?
-    log "This is your first install, creating the postgres database at /usr/local/var/postgres"
-    shell "initdb /usr/local/var/postgres"
-  end
+  after {
+    unless "/usr/local/var/postgres".p.exists?
+      log "This is your first install, creating the postgres database at /usr/local/var/postgres"
+      shell "initdb /usr/local/var/postgres"
+    end
   
-  log_shell "Starting PostgreSQL", "pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
+    log_shell "Starting PostgreSQL", "pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"    
+  }
 end
