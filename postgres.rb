3ee9e6cbad589a0a79df83867b46dfa70a6b127d
Jon Kinney <jonkinney@gmail.com>
2011-09-23 16:06:55 -0500

    start pgsql after installing it

--- postgres.rb
+++ postgres.rb
@@ -8,5 +8,6 @@ dep 'postgres configured' do
   meet {
     log "This is your first install, creating the postgres database at /usr/local/var/postgres"
     shell "initdb /usr/local/var/postgres"
+    log_shell "Starting PostgreSQL", "pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
   }
 end
