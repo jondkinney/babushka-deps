28ab60910f28deda9ee9df7c921e26a3da700383
Jon Kinney <jonkinney@gmail.com>
2011-09-23 16:02:45 -0500

    try to fix mysql up

--- mysql.rb
+++ mysql.rb
@@ -1,8 +1,13 @@
-dep 'mysql', :template => 'managed'
+dep 'mysql.managed' do
+  provides "mysql"
+end
 
 dep 'mysql configured' do
-  requires 'mysql'
-  shell "unset TMPDIR"
-  shell 'sudo mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp'
-  log_shell "Starting MySQL", "mysql.server start"
+  requires 'mysql.managed'
+  met? { "/usr/local/var/mysql".p.exists? }
+  meet {
+    shell "unset TMPDIR"
+    shell 'sudo mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp'
+    log_shell "Starting MySQL", "mysql.server start"
+  }
 end
