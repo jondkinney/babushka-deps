f3d718cd1c637f77d5c98c392b0d1d03494ce90d
Jon Kinney <jonkinney@gmail.com>
2011-09-23 07:19:23 -0500

    remove flip for mac for now

--- app_bundles.rb
+++ app_bundles.rb
@@ -69,9 +69,9 @@ dep 'SmartSleep.app' do
   source 'http://www.jinx.de/SmartSleep_files/SmartSleep.current.dmg'
 end
 
-dep 'Flip4Mac' do
-  source 'http://download.microsoft.com/download/C/8/9/C8951314-E056-404A-8EA8-8744E42594AA/Flip4Mac%20WMV%202.3.8.1.dmg'
-end
+# dep 'Flip4Mac' do
+#   source 'http://download.microsoft.com/download/C/8/9/C8951314-E056-404A-8EA8-8744E42594AA/Flip4Mac%20WMV%202.3.8.1.dmg'
+# end
 
 dep 'Fluid.app' do
   source 'http://fluidapp.com/dist/Fluid_1.2.zip'
