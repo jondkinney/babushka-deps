1eaa489ab0b60359c07138cab7e873c2208a2214
Jon Kinney <jonkinney@gmail.com>
2011-09-23 02:55:10 -0500

    try this simpler mysql

--- mysql.rb
+++ mysql.rb
@@ -2,12 +2,5 @@ dep 'mysql', :template => 'managed'
 
 dep 'mysql configured' do
   requires 'mysql'
-  def my_cnf
-    '/etc/my.cnf'
-  end
-  met? { "/etc/my.cnf".p.exists?  }
-  meet {
-    render_erb 'mysql/my.cnf', :to => my_cnf, :sudo => true
-    shell 'sudo mysql_install_db'
-  }
+  shell 'sudo mysql_install_db --basedir="$(brew --prefix mysql)"'
 end
