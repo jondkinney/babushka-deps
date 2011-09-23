bdf8915871307a3b20d0d7dcf4a9b43c47bcd5b4
Jon Kinney <jonkinney@gmail.com>
2011-09-23 16:47:59 -0500

    dbfixes

--- postgres.rb
+++ postgres.rb
@@ -2,11 +2,8 @@ dep 'postgresql.managed' do
   provides "psql"
   
   after {
-    unless "/usr/local/var/postgres".p.exists?
-      log "This is your first install, creating the postgres database at /usr/local/var/postgres"
-      shell "initdb /usr/local/var/postgres"
-    end
-  
+    log "This is your first install, creating the postgres database at /usr/local/var/postgres"
+    shell "initdb /usr/local/var/postgres"
     log_shell "Starting PostgreSQL", "pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"    
   }
 end
