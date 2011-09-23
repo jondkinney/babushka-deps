a9814f508643361ea4e94a9019848e056076fd5f
Jon Kinney <jonkinney@gmail.com>
2011-09-23 15:09:07 -0500

    fix mysql dep

--- mysql.rb
+++ mysql.rb
@@ -2,5 +2,7 @@ dep 'mysql', :template => 'managed'
 
 dep 'mysql configured' do
   requires 'mysql'
-  shell 'sudo mysql_install_db --basedir="$(brew --prefix mysql)"'
+  shell "unset TMPDIR"
+  shell 'sudo mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp'
+  log_shell "Starting MySQL", "mysql.server start"
 end
