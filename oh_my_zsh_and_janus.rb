5482e805e9813f9772f8fadef05fc3fd34336cc0
Jon Kinney <jonkinney@gmail.com>
2012-03-21 09:30:01 -0500

    Update oh_my_zsh_and_janus.rb

--- oh_my_zsh_and_janus.rb
+++ oh_my_zsh_and_janus.rb
@@ -8,7 +8,7 @@ dep 'oh-my-zsh and janus' do
     shell 'cd ~/.dotfiles; git submodule init; git submodule update;'
     log_shell "Symlinking j2fly_shortcuts.zsh", "ln -nsF /Users/`whoami`/.dotfiles/zsh_conf/custom/j2fly_shortcuts.zsh /Users/`whoami`/.oh-my-zsh/custom/j2fly_shortcuts.zsh"
     log_shell "Symlinking j2fly zsh theme", "ln -nsF /Users/`whoami`/.dotfiles/zsh_conf/j2fly.zsh-theme /Users/`whoami`/.oh-my-zsh/themes/j2fly.zsh-theme"
-    log_shell "Raking Janus... um, yea... TWSS?", "cd ~/.vim;rake"
+    log_shell "Setting up Janus by running Rake", "cd ~/.vim;rake"
     log_shell "Symlinking j2fly.vim colorscheme", "ln -nsF /Users/`whoami`/.dotfiles/vim_conf/colors/j2fly.vim /Users/`whoami`/.vim/colors/j2fly.vim"
     log_shell "Symlinking j2fly.snippets", "ln -nsF /Users/`whoami`/.dotfiles/vim_conf/snippets/j2fly.snippets /Users/`whoami`/.vim/snippets/j2fly.snippets"
     log_shell "Symlinking mvim script to a folder in the path", "ln -nsF /Users/`whoami`/.dotfiles/vim_conf/mvim /usr/local/bin/mvim"
