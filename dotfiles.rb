3df69e52e2b916784e4a345210ae7cb3c5249d13
Jon Kinney <jonkinney@gmail.com>
2014-09-26 17:32:01 -0500

    Update dotfiles

--- dotfiles.rb
+++ dotfiles.rb
@@ -3,14 +3,14 @@ meta 'dotfiles' do
   accepts_value_for :target, :basename
 
   template {
-    met? { 
+    met? {
       if "~/.#{target}/successful_install".p.exists?
         log "There is already a ~/.#{target} installation in place."
         log "Remove the ~/.#{target}/successful_install file and re-run to install again"
         true
       else
         false
-      end  
+      end
     }
     meet {
       if "~/.#{target}".p.exists?
@@ -19,10 +19,10 @@ meta 'dotfiles' do
         end
         log_shell "Backing up the existing ~/.#{target} to ~/.bkp-#{target}", "mv ~/.#{target} ~/.bkp-#{target}"
       end
-      
-      if log_shell "Cloning", "git clone https://github.com/j2fly/#{repo}.git /Users/`whoami`/.#{target}"
+
+      if log_shell "Cloning", "git clone https://github.com/jondkinney/#{repo}.git ~/.#{target}"
         log "Symlinking"
-        shellout = raw_shell("cd /Users/`whoami`/.#{target} && sh install.sh").stdout
+        shellout = raw_shell("cd ~/.#{target} && sh install.sh").stdout
         log "#{shellout}"
       else
         log_shell "Restoring ~/.bkp-#{target} to ~/.#{target}", "mv ~/.bkp-#{target} ~/.#{target}"
