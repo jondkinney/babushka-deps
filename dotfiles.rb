6007fca027edb429a41a24e8350bfdd073302981
Jon Kinney <jonkinney@gmail.com>
2014-12-21 23:19:42 -0600

    Update dotfiles dep

--- dotfiles.rb
+++ dotfiles.rb
@@ -7,20 +7,10 @@ meta 'dotfiles' do
       "~/.#{target}".p.exists?
     }
     meet {
-      if "~/.#{target}".p.exists?
-        if "~/.bkp-#{target}".p.exists?
-          log_shell "Removing backup", "rm -r ~/.bkp-#{target}"
-        end
-        log_shell "Backing up the existing ~/.#{target} to ~/.bkp-#{target}", "mv ~/.#{target} ~/.bkp-#{target}"
-      end
-
-      if log_shell "Cloning", "git clone https://github.com/jondkinney/#{repo}.git ~/.#{target}"
-        log "Symlinking"
-        shellout = raw_shell("cd ~/.#{target} && chmod +x install.sh && sh install.sh").stdout
-        log "#{shellout}"
-      else
-        log_shell "Restoring ~/.bkp-#{target} to ~/.#{target}", "mv ~/.bkp-#{target} ~/.#{target}"
-      end
+      log_shell "Cloning", "git clone https://github.com/jondkinney/#{repo}.git ~/.#{target}"
+      log "Symlinking"
+      shellout = raw_shell("cd ~/.#{target} && chmod +x install.sh && sh install.sh").stdout
+      log "#{shellout}"
     }
   }
 end
