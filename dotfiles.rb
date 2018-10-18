b00beca3cbd7fe2b8b1f4a2e6c481330381f29e9
Jon Kinney <jon@jkmbp.local>
2018-10-18 16:31:26 -0500

    boom

--- dotfiles.rb
+++ dotfiles.rb
@@ -1,7 +1,6 @@
 meta 'dotfiles' do
   accepts_value_for :repo, :basename
   accepts_value_for :target, :basename
-  accepts_value_for :email
 
   template {
     met? {
@@ -10,15 +9,25 @@ meta 'dotfiles' do
     meet {
       log_shell "Cloning", "git clone https://github.com/jondkinney/#{repo}.git ~/.#{target}"
       log "Symlinking"
-      # shellout = raw_shell("cd ~/.#{target} && chmod +x install.sh && bash install.sh \"#{full_name}\" #{email}").stdout
-      shellout = raw_shell("cd ~/.#{target} && chmod +x install.sh && bash install.sh #{email}").stdout
+      shellout = raw_shell("cd ~/.#{target} && chmod +x install.sh && bash install.sh").stdout
       log "#{shellout}"
     }
   }
 end
 
-dep 'dotfiles', :template, :email do
-  template.default('dotfiles')
+dep 'dotfiles', :email do
   email.ask('What is your email?')
+
+  template {
+    met? {
+      "~/.#{target}".p.exists?
+    }
+    meet {
+      log_shell "Cloning", "git clone https://github.com/jondkinney/#{repo}.git ~/.#{target}"
+      log "Symlinking"
+      shellout = raw_shell("cd ~/.#{target} && chmod +x install.sh && bash install.sh #{email}").stdout
+      log "#{shellout}"
+    }
+  }
 end
 dep 'private-dotfiles', template: 'dotfiles'
