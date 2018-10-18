2b9cd0f24e38a006d70bdfc2a5657fddc969f91e
Jon Kinney <jonkinney@gmail.com>
2018-10-18 15:39:49 -0500

    Try to pass values

--- dotfiles.rb
+++ dotfiles.rb
@@ -1,6 +1,8 @@
 meta 'dotfiles' do
   accepts_value_for :repo, :basename
   accepts_value_for :target, :basename
+  accepts_value_for :full_name
+  accepts_value_for :email
 
   template {
     met? {
@@ -9,11 +11,16 @@ meta 'dotfiles' do
     meet {
       log_shell "Cloning", "git clone https://github.com/jondkinney/#{repo}.git ~/.#{target}"
       log "Symlinking"
-      shellout = raw_shell("cd ~/.#{target} && chmod +x install.sh && exec 3<&1;bash <&3 <(install.sh)").stdout
+      shellout = raw_shell("cd ~/.#{target} && chmod +x install.sh && bash install.sh #{full_name} #{email}").stdout
       log "#{shellout}"
     }
   }
 end
 
-dep 'dotfiles', :template => 'dotfiles'
-dep 'private-dotfiles', :template => 'dotfiles'
+dep 'dotfiles', :template, :full_name, :email do
+  template.default('dotfiles')
+  full_name.ask('What is your full name?').default('Jon Kinney')
+  email.ask('What is your email?').default('jonkinney@gmail.com')
+end
+
+dep 'private-dotfiles', template: 'dotfiles'
